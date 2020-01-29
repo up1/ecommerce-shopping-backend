d7f6e6b47e9c7e0004000f7bc2fc2fad5d717736
kanommoji <uracha_p@kkumail.com>
2020-01-29 16:48:24 +0700

    [Edited] แก้ไข unit test เพื่อ ทดสอบ getproductdetail

--- src/test/java/com/tcrb/shoppingcart/product/ProductControllerTest.java
+++ src/test/java/com/tcrb/shoppingcart/product/ProductControllerTest.java
@@ -1,8 +1,10 @@
 package com.tcrb.shoppingcart.product;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +15,9 @@ class ProductControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @MockBean
+    ResponseProduct stubproduct;
+
     @Test
     void getProductList() {
         ResponseProduct result
@@ -20,5 +25,17 @@ class ProductControllerTest {
 
         assertEquals(0,result.getId());
     }
+
+    @Test
+    void getProductByDetail() {
+        stubproduct.setId(1);
+        stubproduct.setName("Balance Trainning Bicycle");
+
+        ResponseProduct result
+                = restTemplate.getForObject("/api/v1/products/1", ResponseProduct.class);
+        assertEquals(1,result.getId());
+        assertEquals("Balance Trainning Bicycle",result.getName());
+    }
+
 
 }
